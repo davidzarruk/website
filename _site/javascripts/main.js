688db50bbe5a62b4ef60e953cbcf3c335a777132
David Zarruk <zarruk@amazon.com>
2023-04-22 13:11:17 -0500

    update

---
+++ _site/javascripts/main.js
@@ -0,0 +1,31 @@
+function customEncodeURIComponent(str) {
+    return encodeURIComponent(str)
+      .replace(/%2F/g, '/')
+      .replace(/%3A/g, ':')
+      .replace(/%3F/g, '?')
+      .replace(/%3D/g, '=')
+      .replace(/%26/g, '&');
+  }
+  
+  document.getElementById('sendButton').addEventListener('click', async () => {
+    const apiUrl = document.getElementById('apiUrl').value;
+    const inputText = document.getElementById('inputText').value;
+    const url = `${apiUrl}/predict/?URL=${customEncodeURIComponent(inputText)}`;
+  
+    try {
+      const response = await fetch(url, { method: 'GET' });
+  
+      if (response.ok) {
+        const data = await response.json();
+        const result = parseFloat(data.result).toFixed(4);
+        document.getElementById('result').innerText = result;
+      } else {
+        console.error('Request failed with status', response.status);
+        document.getElementById('result').innerText = 'Error';
+      }
+    } catch (error) {
+      console.error('Request failed with error', error);
+      document.getElementById('result').innerText = 'Error';
+    }
+  });
+  
